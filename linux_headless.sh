0f1a11ea5850dddeb916c676ed4b7bfade0b59c9
toxtli <ctoxtli@gmail.com>
2019-09-05 14:04:12 -0400

    Headless commands

--- linux_headless.sh
+++ linux_headless.sh
@@ -4,7 +4,7 @@ sudo apt-get install firefox
 sudo apt-get install xvfb
 
 #set display number to :99
-Xvfb :99 -ac &
+nohup Xvfb :99 -ac &
 export DISPLAY=:99    
 
 #you are now having an X display by Xvfb
