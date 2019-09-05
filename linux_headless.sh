e7882d549637277db8b96bbdcfe75cfebc55592c
toxtli <ctoxtli@gmail.com>
2019-09-05 14:24:56 -0400

    Headless commands

--- linux_headless.sh
+++ linux_headless.sh
@@ -7,6 +7,9 @@ sudo apt-get install xvfb
 #set display number to :99
 nohup Xvfb :99 -ac &
 export DISPLAY=:99    
-
 #you are now having an X display by Xvfb
-
+#
+pip install -r requirements.txt
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+tar -xvf geckodriver-v0.24.0-linux64.tar.gz
+cp geckodriver /usr/local/bin/
